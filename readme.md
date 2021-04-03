8741192e9bb1fdc3f06ef429a9df53b7fa616c80
Georgy Bunin <bunin.co.il@gmail.com>
2021-04-03 10:42:09 +0300

    add example

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Smart Games Iq Fit Solutions
+
+## Description
+
+Here you can find first 1000 solutions for IQ Fit puzzle.
+
--- smart-games-iq-fit-soultions-client/src/App.js
+++ smart-games-iq-fit-soultions-client/src/App.js
@@ -1,11 +1,14 @@
-import Solution from "./Solution";
 import React from "react";
+import * as styles from './App.module.css';
+import Solution from "./Solution";
+
 const data = require('./data.json');
 
 function App() {
     return <>
+        <h1>Smart Games IQ Fit (first 1000 solutions)</h1>
         {data.map((figures, i) => (
-            <div key={i}>
+            <div key={i} className={styles.solutionBlock}>
                 <h4>Solution # {i + 1}</h4>
                 <Solution figures={figures}/>
             </div>
